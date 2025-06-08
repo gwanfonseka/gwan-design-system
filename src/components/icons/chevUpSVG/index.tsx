d10f6ac3d66ecb045c9c9f01aea3efe635901924
gwanfonseka <uidev.nimeshfonseka@gmail.com>
2025-06-08 13:18:54 +0800

    feat: new icons added

---
+++ src/components/icons/chevUpSVG/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+const ChevUp = (): ReactNode => {
+  return (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="m17.635,14.428l-5.281-5.281c-.189-.189-.518-.189-.707,0l-5.281,5.281-.707-.707,5.281-5.281c.566-.566,1.555-.566,2.121,0l5.281,5.281-.707.707Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export { ChevUp };
