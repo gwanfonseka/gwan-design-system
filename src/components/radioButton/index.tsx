39cf675d756135fdb01d2dd281f0fdd35583ff0a
Shemeera Fonseka <shemeerafonseka@gmail.com>
2025-11-18 14:55:28 +0530

    feature/50-feature-implement-disabled-states

--- src/components/radioButton/index.tsx
+++ src/components/radioButton/index.tsx
@@ -6,6 +6,7 @@ export interface IRadioButton {
   selectedValue?: string;
   onChange?: (value: string) => void;
   className?: string;
+  disabled?: boolean;
 }
 
 const RadioButton: FC<IRadioButton> = ({
@@ -14,20 +15,27 @@ const RadioButton: FC<IRadioButton> = ({
   selectedValue,
   onChange,
   className = "",
+  disabled = false,
 }: IRadioButton) => {
   const isChecked = selectedValue === value;
 
+  const handleClick = () => {
+    if (disabled) return;
+    if (onChange) onChange(value);
+  }
+
   return (
-    <label className={`flex items-center gap-2 cursor-pointer ${className}`}>
+    <label className={`flex items-center gap-2 cursor-pointer ${className}
+    ${disabled ? "cursor-not-allowed opacity-50" : ""}`}>
       <div
         className={`w-5 h-5 flex items-center justify-center border-2 rounded-full transition-all
-            ${
-              isChecked
-                ? "border-primary-500 bg-primary-500"
-                : "border-gray-400 bg-white"
-            }
+            ${isChecked
+            ? "border-primary-500 bg-primary-500"
+            : "border-gray-400 bg-white"
+          }
+             ${disabled ? "bg-gray-200 border-gray-300" : ""}
           `}
-        onClick={() => onChange && onChange(value)}
+        onClick={handleClick}
       >
         {isChecked && <div className="w-2.5 h-2.5 bg-white rounded-full"></div>}
       </div>
@@ -38,6 +46,7 @@ const RadioButton: FC<IRadioButton> = ({
         type="radio"
         value={value}
         checked={isChecked}
+        disabled={disabled}
         onChange={() => onChange && onChange(value)}
         className="hidden"
       />
