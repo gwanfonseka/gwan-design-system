e7084023382bece4428f5be08456ac6512fc73ba
gwanfonseka <uidev.nimeshfonseka@gmail.com>
2025-12-06 19:38:20 +0800

    feat: new icons

---
+++ src/components/icons/pinSVG/index.tsx
@@ -0,0 +1,14 @@
+const Pin = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        fill="currentColor"
+        d="M18,6c0-3.309-2.691-6-6-6S6,2.691,6,6c0,3.14,2.425,5.724,5.5,5.979v11.521c0,.276,.224,.5,.5,.5s.5-.224,.5-.5V11.979c3.075-.255,5.5-2.839,5.5-5.979Zm-6,5c-2.757,0-5-2.243-5-5S9.243,1,12,1s5,2.243,5,5-2.243,5-5,5Z"
+      />
+    </svg>
+  );
+};
+
+export { Pin };
