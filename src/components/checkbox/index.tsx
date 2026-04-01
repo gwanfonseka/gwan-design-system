4d8ab0a2253afa69f3581d88df97e6280a9d7d5b
gwanfonseka <uidev.nimeshfonseka@gmail.com>
2026-04-01 08:33:29 +0800

    feat/44: codesnippet and playground for checkbox

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,13 +1,46 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { CheckSVG } from "../icons";
+
+export enum CHECKBOX_SIZE {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
+}
+
+export enum CHECKBOX_EDGE_STYLE {
+  ROUNDED = "rounded",
+  SQUARED = "squared",
+}
 
 export interface ICheckbox {
   label: string;
   checked: boolean;
   onChange: (checked: boolean) => void;
   className?: string;
-  disabled?:boolean;
+  disabled?: boolean;
+  size?: CHECKBOX_SIZE;
+  edges?: CHECKBOX_EDGE_STYLE;
 }
+
+const sizeMap = {
+  [CHECKBOX_SIZE.SMALL]: {
+    box: "size-4",
+    icon: "size-3",
+  },
+  [CHECKBOX_SIZE.MEDIUM]: {
+    box: "size-5",
+    icon: "size-4",
+  },
+  [CHECKBOX_SIZE.LARGE]: {
+    box: "size-6",
+    icon: "size-5",
+  },
+};
+
+const edgeMap = {
+  [CHECKBOX_EDGE_STYLE.ROUNDED]: "rounded-sm",
+  [CHECKBOX_EDGE_STYLE.SQUARED]: "rounded-none",
+};
 
 const Checkbox: FC<ICheckbox> = ({
   label,
@@ -15,45 +48,43 @@ const Checkbox: FC<ICheckbox> = ({
   onChange,
   className = "",
   disabled = false,
+  size = CHECKBOX_SIZE.MEDIUM,
+  edges = CHECKBOX_EDGE_STYLE.ROUNDED,
 }: ICheckbox) => {
-  const [isChecked, setIsChecked] = useState(checked);
-
-  const handleToggle = () => {
-    if (disabled) return;
-
-    setIsChecked(!isChecked);
-    if (onChange) {
-      onChange(!isChecked);
-    }
-  };
+  const { box, icon } = sizeMap[size];
+  const edgeStyle = edgeMap[edges];
 
   return (
-    <label className={`flex items-center gap-2 cursor-pointer ${className} ${
+    <label
+      className={`flex items-center gap-2 cursor-pointer ${className} ${
         disabled ? "cursor-not-allowed opacity-50" : ""
-      }`}>
+      }`}
+    >
       <input
         type="checkbox"
-        checked={isChecked}
-        onChange={handleToggle}
+        checked={checked}
+        onChange={() => !disabled && onChange(!checked)}
         disabled={disabled}
         className="hidden"
       />
+
       <div
-        className={`w-5 h-5 flex items-center justify-center border-2 rounded-md transition-all 
+        className={`${box} ${edgeStyle} flex items-center justify-center border transition-all 
           ${
-            isChecked
+            checked
               ? "bg-primary-300 border-primary-400"
               : "bg-white border-neutral-500"
           }
           ${disabled ? "bg-neutral-200 border-neutral-400" : ""}
         `}
       >
-        {isChecked && (
-          <div className="size-4 text-black">
+        {checked && (
+          <div className={`${icon} text-black`}>
             <CheckSVG />
           </div>
         )}
       </div>
+
       {label && <span className="text-black">{label}</span>}
     </label>
   );
